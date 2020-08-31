6f839223f5fab20921d1b97c6ac07ca85f38f437
Mihail <zadoromihail@gmail.com>
2020-08-31 13:06:44 +0300

    initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Михаил Задорожный on 31.08.2020.
+//  Copyright © 2020 Михаил Задорожный. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
