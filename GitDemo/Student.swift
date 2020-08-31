3ec8749ec51a1cff9171efe6dd335468256b8ec4
Mihail <zadoromihail@gmail.com>
2020-08-31 18:10:36 +0300

    add struct Student

---
+++ GitDemo/Student.swift
@@ -0,0 +1,14 @@
+//
+//  Student.swift
+//  GitDemo
+//
+//  Created by Михаил Задорожный on 31.08.2020.
+//  Copyright © 2020 Михаил Задорожный. All rights reserved.
+//
+
+import Foundation
+
+struct Student {
+    var name: String
+    var age: String
+}
